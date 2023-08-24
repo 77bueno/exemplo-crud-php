d5cf5057d2e7bb95550b4bb1477349c065943f7e
victor bueno <victorhugomotabueno@gmail.com>
2023-08-24 11:45:09 -0300

    exercicio tabela com dados do banco de dados

--- fabricantes/visualizar.php
+++ fabricantes/visualizar.php
@@ -1,9 +1,47 @@
+<?php
+/* Importando as funções de manipulação de fabricantes */
+require_once "../src/funcoes-fabricantes.php";
+
+/* Guardando o retorno/resultado da função lerFabricantes */
+$listaDeFabricantes = lerFabricantes($conexao);
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fabricantes - Visualização</title>
+    <style>
+        /* Estilos para a tabela */
+        .styled-table {
+            width: 40%; /* Largura da tabela */
+            border-collapse: collapse;
+            border: 1px solid #ddd;
+            background-color: #e6f7ff;
+        }
+
+        /* Estilos para as células do cabeçalho */
+        .styled-table th {
+            padding: 6px; /* Reduzindo o preenchimento */
+            font-size: 16px; /* Reduzindo o tamanho da fonte */
+            text-align: left;
+            background-color: #b3d9ff;
+            border-bottom: 1px solid #ddd;
+        }
+
+        /* Estilos para as células normais */
+        .styled-table td {
+            padding: 6px; /* Reduzindo o preenchimento */
+            font-size: 16px; /* Reduzindo o tamanho da fonte */
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+        /* Estilos para realçar as linhas ao passar o mouse */
+        .styled-table tr:hover {
+            background-color: #cce0ff;
+        }
+    </style>
 </head>
 <body>
     <h1>Fabricantes | SELECT 
@@ -14,5 +52,29 @@
     <h2>Lendo e carregando todos os fabricantes</h2>
 
     <p><a href="inserir.php">Inserir novo fabricante</a></p>
+
+    <table  class="styled-table">
+        <caption>Lista de Fabricantes</caption>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Operações</th>
+            </tr>
+        </thead>
+<?php 
+    foreach ($listaDeFabricantes as $fabricante) {
+?>   
+        <tbody>
+            <tr>
+                <td><?=$fabricante['id']?></td>
+                <td><?=$fabricante['nome']?></td>
+                <td><a href="">Editar</a> <a href="">Excluir</a></td>
+            </tr>
+        </tbody>
+<?php
+}
+?>
+    </table>
 </body>
 </html>
--- src/funcoes-fabricantes.php
+++ src/funcoes-fabricantes.php
@@ -28,5 +28,5 @@ function lerFabricantes( PDO $conexao ){
 }
 
 // Teste
-$dados = lerFabricantes($conexao);
-var_dump($dados);
+// $dados = lerFabricantes($conexao);
+// var_dump($dados);
