9f2588f3e42bc0b7eae68e0d95379ae853fe0a7f
victor bueno <victorhugomotabueno@gmail.com>
2023-08-25 08:55:34 -0300

    Alterações no inserir

--- fabricantes/inserir.php
+++ fabricantes/inserir.php
@@ -1,9 +1,22 @@
+<?php
+/* Verificando se o formulário/botão foi acionado */ 
+if(isset($_POST['inserir']) ) {
+    // Capturando o valor digitado do nome e sanitizando
+    $nome = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS); 
+    
+    // Pode ser assim também:
+    // $nome = filter_var($_POST['nome'], FILTER_SANITIZE_SPECIAL_CHARS); 
+
+    echo $nome;
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fabricantes - Inserção</title>
+    
 </head>
 <body>
     <h1>Fabricantes | INSERT</h1>
@@ -11,9 +24,9 @@
     <form action="" method="post">
         <p>
             <label for="nome">Nome:</label>
-            <input type="text" name="nome" id="nome">
+            <input required type="text" name="nome" id="nome">
         </p>
-        <button type="submit">Inserir fabricante</button>
+        <button type="submit" name="inserir">Inserir fabricante</button>
     </form>
 
 </body>
