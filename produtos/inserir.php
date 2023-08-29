028b98bbdc59a670963ba4cd5686affd1aee32ac
victor bueno <victorhugomotabueno@gmail.com>
2023-08-29 11:40:19 -0300

    criando o form, e fazendo o exercicio

--- produtos/inserir.php
+++ produtos/inserir.php
@@ -1,3 +1,8 @@
+<?php
+require_once "../src/funcoes-fabricantes.php"; 
+
+$fabricantes = lerFabricantes($conexao);
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -9,7 +14,36 @@
     <h1>Produtos | INSERT</h1>
     <hr>
 
+    <form action="" method="post">
+        <p>
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome" required>
+        </p>
+        <p>
+            <label for="preco">Preço:</label>
+            <input type="number" min="10" max="10000" step="0.01" name="preco" id="preco" required>
+        </p>
+        <p>
+            <label for="quantidade">Quantidade:</label>
+            <input type="number" min="1" max="100" name="quantidade" id="quantidade" required>
+        </p>
+        <p>
+            <label for="fabricante">Fabricante</label>
+            <select name="fabricante" id="fabricante" required>
+                <option value=""></option>
+                <?php foreach ($fabricantes as $fabricante) { ?> 
+                    <option value="<?=$fabricante["id"]?>"><?=$fabricante["nome"]?></option>
+                <?php } ?>
+            </select>
+        </p>
+        <p>
+            <label for="descricao">Descrição:</label> <br>
+            <textarea name="descricao" id="descricao" cols="30" rows="10"></textarea>
+        </p>
+        <button type="submit" name="inserir">Inserir produto</button>
+    </form>
+
     <hr>
-    <p><a href=""></a></p>
+    <p><a href="visualizar.php">Voltar</a></p>
 </body>
 </html>
--- produtos/visualizar.php
+++ produtos/visualizar.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once "../src/funcoes-produtos.php";
 require_once "../src/funcoes-utilitarias.php";
 
@@ -11,12 +11,16 @@ $listaDeProdutos = lerProdutos($conexao);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produtos - Visualização</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <style>
-        * { box-sizing: border-box; }
+        * {
+            box-sizing: border-box;
+        }
 
         body {
             background-color: #b3d9ff;
         }
+
         .produtos {
             font-family: 'Segoe UI';
             display: flex;
@@ -27,20 +31,20 @@ $listaDeProdutos = lerProdutos($conexao);
         }
 
         .produto {
-            background-color: #0056b3;
             text-align: center;
-            color: white;
+            color: black;
             font-weight: bold;
-            text-shadow: black 1px 1px 1px;
             padding: 1rem;
             width: 49%;
             box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
         }
 
-        hr { width: 30%; }
-        
+        .card-header {
+            background-color: lightsteelblue;
+        }
     </style>
 </head>
+
 <body>
     <h1>Produtos | SELECT - <a href="../index.php">Home</a></h1>
 
@@ -49,17 +53,28 @@ $listaDeProdutos = lerProdutos($conexao);
     <p><a href="inserir.php">Inserir novo produto</a></p>
 
     <div class="produtos">
-        <?php foreach ( $listaDeProdutos as $produto ) { ?>
-        
+        <?php foreach ($listaDeProdutos as $produto) { ?>
+
             <article class="produto">
-                <h3> <?=$produto["produto"]?> </h3>
-                <h4> <?=$produto["fabricante"]?> </h4>
-                <hr>
-                <p ><b>Preço:</b> <?=formatarPreco($produto["preco"])?></p>
-                <p><b>Quantidade:</b> <?=$produto["quantidade"]?></p>
-                <p><b>Total: </b> <?=calcularTotal($produto["preco"], $produto["quantidade"])?></p>
+            <div class="card">
+                <div class="card-header">
+                    <h3> <?= $produto["produto"] ?> </h3>
+                    <h4> <?= $produto["fabricante"] ?> </h4>
+                </div>
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                        <p><b>Preço:</b> <?= formatarPreco($produto["preco"]) ?></p>
+                        <p><b>Quantidade:</b> <?= $produto["quantidade"] ?></p>
+                        <p><b>Total: </b> <?= calcularTotal($produto["preco"], $produto["quantidade"]) ?></p>
+                    </blockquote>
+                </div>
+            </div>
             </article>
+           
+
         <?php } ?>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
