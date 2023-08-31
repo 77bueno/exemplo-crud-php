f07c5a651c8591dd3abd54903e239c38e4f7a829
victor bueno <victorhugomotabueno@gmail.com>
2023-08-31 09:52:01 -0300

    criando a função excluir

--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -107,3 +107,15 @@ function atualizarProduto(
         die("Erro ao atualizar: ".$erro->getMessage());
     }
 }
+
+function excluirProduto(PDO $conexao, int $id):void {
+    $sql = "DELETE FROM produtos WHERE id = :id";
+
+    try {
+        $consulta = $conexao->prepare($sql);
+        $consulta->bindValue(":id", $id, PDO::PARAM_INT);
+        $consulta->execute();
+    } catch (Exception $erro) {
+        die("Erro ao excluir: ".$erro->getMessage());
+    }
+}
